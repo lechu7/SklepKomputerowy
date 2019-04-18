11ab85a49eaa2edca2203a22f3e7a079153c268b
lechu7 <lechu0@vp.pl>
2019-04-18 16:29:56 +0200

    Indeksy

---
+++ index/index.sql
@@ -0,0 +1,8 @@
+CREATE INDEX index_nazwa_towar
+ON Towar (Nazwa);
+
+CREATE INDEX index_id_punkt_odbioru
+ON PunktOdbioru (Id_Punkt_Odbioru);
+
+CREATE INDEX index_nazwa_dostawcy
+ON Dostawcy (nazwa);
